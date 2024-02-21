1559cc493cada33e98e0393cad5733b739e0fb99
Diakivakoko <wowova@bk.ru>
2024-02-21 20:44:24 +0700

    similar to registration in PostgresSQL

--- database/database.py
+++ database/database.py
@@ -1,26 +1,93 @@
-import sqlite3
-
-db = sqlite3.connect('../server.db')
-sql = db.cursor()
-
-sql.execute("""CREATE TABLE IF NOT EXISTS users (
-    role TEXT,
-    username TEXT,
-    password TEXT
-)""")
-db.commit()
+import psycopg2
+from psycopg2 import Error
 
 
-def get_users_db() -> list:
-    items = []
-    for value in sql.execute("SELECT * FROM users"):
-        items.append(value)
+def create_users_table():
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="postgresosikati",
+            host="localhost",
+            port="5432",
+            database="psycho"
+        )
 
-    return items
+        cursor = connection.cursor()
 
-def register_db(username, password) -> None:
-    sql.execute(f"SELECT username FROM users WHERE username = '{username}'")
-    role = "user"
-    if sql.fetchone() is None:
-        sql.execute("INSERT INTO users VALUES (?, ?, ?)", (role, username, password))
-        db.commit()
+        create_table_query = '''CREATE TABLE IF NOT EXISTS users (
+                                id SERIAL PRIMARY KEY,
+                                username VARCHAR(50) UNIQUE NOT NULL,
+                                password VARCHAR(50) NOT NULL); '''
+
+        cursor.execute(create_table_query)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+
+
+def register_user(username, password):
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="postgresosikati",
+            host="localhost",
+            port="5432",
+            database="psycho"
+        )
+
+        cursor = connection.cursor()
+
+        insert_query = """INSERT INTO users (username, password) VALUES (%s, %s)"""
+        record_to_insert = (username, password)
+
+        cursor.execute(insert_query, record_to_insert)
+        connection.commit()
+        print("Пользователь успешно зарегистрирован")
+        cursor.close()
+        connection.close()
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+
+
+# Пример использования функций
+create_users_table()
+
+
+def get_all_users():
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="postgresosikati",
+            host="localhost",
+            port="5432",
+            database="psycho"
+        )
+
+        cursor = connection.cursor()
+
+        select_query = """SELECT * FROM users"""
+
+        cursor.execute(select_query)
+
+        # Получение результатов запроса
+        users = cursor.fetchall()
+
+        # Вывод результатов
+        user_list = []
+        user_item = []
+        for user in users:
+            user_item.append(user[0])
+            user_item.append(user[1])
+            user_item.append(user[2])
+            user_list.append(user_item)
+            user_item = []
+
+        cursor.close()
+        connection.close()
+        return user_list
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
--- services/users.py
+++ services/users.py
@@ -1,16 +1,16 @@
 from schemas.users import Creds
-from database.database import get_users_db, register_db
+from database.database import register_user, get_all_users
 
 class UserServise:
 
     def get_users(self) -> list:
-        items = get_users_db()
+        items = get_all_users()
 
         return items
 
 
     def register(self, payload: Creds) -> None:
-        register_db(payload.username, payload.password)
+        register_user(payload.username, payload.password)
 
 
 
