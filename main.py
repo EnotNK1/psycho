4796d07f816e711d4269bb6b97af72e1434beac8
Diakivakoko <wowova@bk.ru>
2024-03-13 23:49:22 +0700

    nginx

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ app.add_middleware(
 )
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=80)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8080)
