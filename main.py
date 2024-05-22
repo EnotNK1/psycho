998b816ab56bebd649825ab1818e61a2d2148fe9
Diakivakoko <wowova@bk.ru>
2024-05-22 19:04:38 +0700

    Возможно работает / возможно нет

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ app.include_router(router)
 #     "http://localhost:3000",
 # ]
 #
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-#     allow_headers=["*"]
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=["*"]
+)
 
 # if __name__ == '__main__':
 #     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8080)
