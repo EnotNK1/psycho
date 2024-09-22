ea4888bfe29a015e9a8663397208adfee988c0af
Diakivakoko <wowova@bk.ru>
2024-09-22 15:12:22 +0700

    Исправлено создание моделей и удалены ненужные миграции

--- entrypoint.py
+++ entrypoint.py
@@ -5,6 +5,7 @@ from database.services.users import user_service_db
 from database.test_info import Test_maslach, Test_DASS, Test_STAI, Test_coling_strategy, Test_cmq, Test_jas, Test_bek21
 from database.edu_info import Base, Cbt_base, Burnout, Breathing_techniques, Relaxation_techniques, Coping_strategies
 
+user_service_db.create_tables()
 create_service_db.create_inquirty()
 create_service_db.create_type_analysis()
 create_service_db.create_test(Test_maslach)
