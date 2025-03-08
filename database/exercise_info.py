c60640afc1bf8324eca76981221ef373658b14de
isla <mislambek7@gmail.com>
2025-03-08 11:39:53 +0700

    Вернул предыдущую версию кпт-дневника

--- database/exercise_info.py
+++ database/exercise_info.py
@@ -29,7 +29,7 @@ class Cpt_diary():
         {
             'title': "Какие чувства вызвала у вас эта ситуация",
             'description': "Опишите чувства, которые возникли. Оцени их уровень на слайдере ниже.",
-            'hint': "Тест",
+            'hint': "Тестт",
             'type': FieldType.TEXT,
             'major': False,
             'variants': [],
